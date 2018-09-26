3f2c08bc0f1472aeac5ee3418ee87784ed64409e
Richard Sjöberg <richard.sjoberg@live.com>
2018-09-26 17:32:07 +0200

    test

--- Tradlite.IgPocBot/CandleUpdatedEventArgs.cs
+++ Tradlite.IgPocBot/CandleUpdatedEventArgs.cs
@@ -6,7 +6,7 @@ using Trady.Core.Infrastructure;
 namespace Tradlite.IgPocBot
 {
     public class CandleUpdatedEventArgs : EventArgs
-    {
+    { //test 
         public IOhlcv Candle { get; set; }
         public string Ticker { get; set; }
         public bool NewCandle { get; set; }
